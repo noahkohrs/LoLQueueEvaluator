a8a2af8c57b64fbd5529a4e04c3f2b6861893af3
noahkohrs <noah@kohrs.fr>
2024-01-27 23:37:34 +0100

    FIX | Improved error management

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # LoL Queue Calculator
 
 ## Overview
-This Python tool is designed for League of Legends (LoL) players to analyze the concept of winner and loser queues. It calculates and interprets history to determine if a player is likely in a winner's queue (where the chances of winning are higher due to various factors) or a loser's queue (where the opposite is true). By assessing recent game statistics such as win rates, team performance, and player behaviors, the tool aims to provide insights into a player's current matchmaking status.
+This Python tool is designed for League of Legends (LoL) players to analyze the concept of winner and loser queues. It calculates and interprets history to determine if a player is likely in a winner's queue (where the chances of winning are higher due to unknown factors) or a loser's queue. By assessing recent game win rates the tool aims to provide insights into a player's current matchmaking status.
+
 
 ## Installation
 
@@ -18,27 +19,35 @@ This Python tool is designed for League of Legends (LoL) players to analyze the 
    YOUR_LOL_API_KEY='<your-api-key-here>'
    ```
 
-## Credits
-
-Thanks to [Riot Games](https://developer.riotgames.com/) for providing the API used in this project.
-
 ## Usage
 
-Run the script and interact with the GUI.
+Run the script.
 
 Example:
 ```python
 python main.py
 ```
 
+This will open a GUI that looks like this:
+
+![GUI](UI_img.png)
+
+
+## Credits
+
+Thanks to [Riot Games](https://developer.riotgames.com/) for providing the API used in this project.
+
+
+
 ## Authors
 - [Arthur Mermet](https://github.com/DNSJambon)
 - [Noah Kohrs](https://github.com/noahkohrs)
 
 
-
 ## Disclaimers
 
 This is a prototype and is not intended for serious use.
+
+This project only currently works for EUW players. Feel free to fork and add support for other regions.
 
 This is a personal project and is not endorsed by Riot Games. It is created for educational purposes.
--- main.py
+++ main.py
@@ -38,8 +38,11 @@ def calculateAction():
     n: int = int(sessions_spinbox.get())
     starting_game: int = int(starting_game_spinbox.get())
     tag = player_name + "-" + str(n) + "-" + str(starting_game)
-
-    WinLoss = calculateWins(player_name, n, starting_game)
+    try:
+        WinLoss = calculateWins(player_name, n, starting_game)
+    except:
+        messagebox.showerror("Error", "Please verify that you have entered a valid key in the .env file and that the player name is correct")
+        return
     AllyWinrate = WinLoss[0]/(n*n*4)*100
     EnemyWinrate = WinLoss[1]/(n*n*5)*100
     BinAlly = calculateBinomialProbability(WinLoss[0], n*n*4)
@@ -49,17 +52,17 @@ def calculateAction():
     FinalResult = calculateBinomialProbability(cumulativeSuccesses, totalNumberOfGames)
 
     Conclusion = "\n"
-    if FinalResult < 0.01:
+    if FinalResult < 0.006:
         if cumulativeSuccesses >= (n*n*9)*0.5:
             Conclusion = "You litteraly can't lose next game"
         else:
             Conclusion = "Wtf these game were wild 💀💀💀"
-    elif FinalResult < 0.1:
+    elif FinalResult < 0.07:
         if cumulativeSuccesses >= (n*n*9)*0.5:
             Conclusion = "You're surely in winner's queue"
         else:
             Conclusion = "You're surely in looser's queue"
-    elif FinalResult < 0.22:
+    elif FinalResult < 0.2:
         if cumulativeSuccesses >= (n*n*9)*0.5:
             Conclusion = "You're probably in winner's queue"
         else:
@@ -99,7 +102,7 @@ root.title("LoL Queue Calculator")
 main_frame = tk.Frame(root)
 main_frame.pack(fill=tk.BOTH, expand=True)
 
-# Create a PanedWindow widget as a container for the input and result sections
+# PanedWindow
 paned_window = ttk.PanedWindow(main_frame, orient=tk.HORIZONTAL)
 paned_window.pack(fill=tk.BOTH, expand=True)
 
@@ -115,15 +118,15 @@ player_name_entry = tk.Entry(input_frame)
 player_name_entry.pack()
 
 # Number of sessions input
-sessions_label = tk.Label(input_frame, text="Number of sessions :")
+sessions_label = tk.Label(input_frame, text="Number of game recursions :")
 sessions_label.pack()
-sessions_spinbox = tk.Spinbox(input_frame, from_=1, to=7)
+sessions_spinbox = tk.Spinbox(input_frame, from_=1, to=5)
 sessions_spinbox.pack()
 
 # Starting game input
 starting_game_label = tk.Label(input_frame, text="From which game :")
 starting_game_label.pack()
-starting_game_spinbox = tk.Spinbox(input_frame, from_=0, to=7)
+starting_game_spinbox = tk.Spinbox(input_frame, from_=0, to=10)
 starting_game_spinbox.pack()
 
 # Calculate button
